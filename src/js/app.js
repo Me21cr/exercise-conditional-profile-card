0a87b58d20ed227e0356ca9c6b9f9c0a8511487f
Me21cr <128864515+Me21cr@users.noreply.github.com>
2023-07-28 01:18:36 +0000

    final

--- src/js/app.js
+++ src/js/app.js
@@ -46,17 +46,20 @@ function render(variables = {}) {
               ? ""
               : variables["socialMediaPosition"]
           } ">
-            <li><a href="https://twitter.com/ ${
+            <li><a href="https://twitter.com/${
               variables["twitter"] == null ? "" : variables["twitter"]
             } "><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/ ${
+            <li><a href="https://github.com/${
               variables["github"] == null ? "" : variables["github"]
             }"><i class="fab fa-github"></i></a></li>
-            <l">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          
+          
+            <li><a href="https://linkedin.com/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -77,7 +80,7 @@ window.onload = function() {
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
